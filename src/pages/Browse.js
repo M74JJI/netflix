b35914f76d73dd8a8484e58ccf6772e6efd9251c
MOHAMED HAJJI <pytaek1@gmail.com>
2022-05-09 19:34:02 +0100

    first commitk

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { BrowseContainer } from '../containers/browse';
-import { UseContent } from '../hooks';
-import selectionFilter from '../utils/selection-filter';
+import React from "react";
+import { BrowseContainer } from "../containers/browse";
+import { UseContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 function Browse() {
-    const { series } = UseContent('series');
-    const { films } = UseContent('films');
+  const { series } = UseContent("series");
+  const { films } = UseContent("films");
 
-    const slides = selectionFilter({ series, films });
-    console.log(slides);
-    return <BrowseContainer slides={slides} />;
+  const slides = selectionFilter({ series, films });
+  return <BrowseContainer slides={slides} />;
 }
 
 export default Browse;
